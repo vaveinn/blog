4b278daf46bd5d4e17a5706e4ed0dd2c12552a60
vaveinn <vaveinn@outlook.com>
2025-08-14 11:09:22 +0800

    f2

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Lucas",
+	bio: "且听风吟，静待花开。",
 	links: [
 		{
 			name: "Twitter",
